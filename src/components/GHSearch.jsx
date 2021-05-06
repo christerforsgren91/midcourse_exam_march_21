c8937d186fb4a780b32d2fb86c25c4227d94385a
Christer <christerforsgren91@gmail.com>
2021-05-06 12:07:51 +0200

    updated test file imported axios

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Button, Input } from 'semantic-ui-react'
+import axios from 'axios'
 
 const GHSearch = () => {
+ const [userName, setUserName] = useState()
+
+
+const userInfo = async () => {
+  let response = await axios.get('/search/users/')
+
+}
+
+
+
+
+
   return (
     <>
       <Input data-cy="input-name" type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
+      <Button data-cy="search-button" name="search" onClick>Search</Button>
     </>
   )
 }
